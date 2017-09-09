8bcd5d5d10c5440525e4e0d4b4c1d50815e6fe60
loezzje <m.demmendaal@hotmail.com>
2017-09-09 13:40:44 +0200

    set up view page artist

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -2,4 +2,15 @@ class ArtistsController < ApplicationController
   def index
     @artists = Artist.all
   end
+  def show
+    @artist = Artist.find(params[:id])
+  end
+
+
+
+  # def artist_params
+  #   params
+  #   .require(:artist)
+  #   .permit(:name, song_ids: [])
+  # end
 end
