ac48018590d80ac265cef871810b00ba2dddc4b4
loezzje <m.demmendaal@hotmail.com>
2017-09-10 11:57:28 +0200

    add things to try to create new song, need to fix later

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,3 +1,33 @@
 class SongsController < ApplicationController
-  
+
+  def index
+    artist = Artist.find(params[:artist_id])
+    @song = artist.songs
+  end
+
+def show
+  artist = Artist.find(params[:artist_id])
+  @song = artist.songs.find(params[:id])
+end
+
+  def new
+    artist = Artist.find(params[:artist_id])
+    @song = artist.songs.build
+  end
+
+  def create
+    artist = Artist.find(params[:artist_id])
+    @song = artist.songs.create(params[:song])
+  end
+
+
+private
+
+def song_params
+  params
+  .require(:song, :artist_id)
+  .permit(:name)
+
+end
+
 end
