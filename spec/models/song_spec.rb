058c2696374808c56d66b0c09b0ec70178a36a39
loezzje <m.demmendaal@hotmail.com>
2017-09-14 16:56:08 +0200

    renamed spec file songs

--- spec/models/song_spec.rb
+++ spec/models/song_spec.rb
@@ -4,24 +4,22 @@ require 'rails_helper'
 RSpec.describe Song, type: :model do
 
   describe "validations" do
-
     it "is invalid without name" do
       song = Song.new(name:"")
       song.valid?
       expect(song.errors).to have_key(:name)
     end
     #if time add uniqueness to songs
-
-
   end
 
   describe "association with artist" do
     let(:artist) { create :artist }
-    
+
     it "belongs to artist" do
       song = artist.songs.new
 
       expect(song.artist).to eq(artist)
     end
   end
+
 end
