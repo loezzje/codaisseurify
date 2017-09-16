9f56f561db151795f83c80c7137c70a418c8e4bb
loezzje <m.demmendaal@hotmail.com>
2017-09-16 17:40:48 +0200

    delete song jquery, not yet ajax

--- app/assets/javascripts/songs.js
+++ app/assets/javascripts/songs.js
@@ -3,6 +3,25 @@ function toggleToBeDelete() {
   $(checkbox).parent().toggleClass("toBeDeleted");
 }
 
+// function submitSong() {
+//   event.preventDefault();
+//   var title = $("#new-song").val();
+//
+//   createSong(title);
+//   $("#new-song").val(null);
+// }
+//
+// function createSong(title) {
+//   var checkboxId = "song-" +
+// }
+
+function deleteSelectedSongs(event) {
+  event.preventDefault();
+  $.when($(".toBeDeleted").remove())
+}
+
 $(document).ready(function() {
   $("input[type=checkbox]").bind('change', toggleToBeDelete);
+  $("#delete").bind('click', deleteSelectedSongs);
+  // $("form").bind('submit', submitSong);
 });
